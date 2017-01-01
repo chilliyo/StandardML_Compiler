82fa909b54cfec1b2ce6573ac4d2dd12f6fbe3cd
Corin Pitcher <cpitcher@cs.depaul.edu>
2017-01-01 15:29:55 -0600

    Initial commit.

---
+++ src/main/scala/Experiments.scala
@@ -0,0 +1,11 @@
+object Experiments {
+  def main (args : Array[String]) : Unit = {
+    println ("Hello world")
+
+    // println ("Uncomment me for more")
+
+    /*
+     println ("Or you can use C-style comments")
+     */
+  }
+}
